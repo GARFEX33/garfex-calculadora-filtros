f14905df4cbdc4dc1bb15f26e0f965e561b822b7
GARFEX33 <ingenieriagarfex33@gmail.com>
2026-02-12 20:45:37 -0700

    feat: add Temperatura enum

---
+++ internal/domain/valueobject/temperatura_test.go
@@ -0,0 +1,25 @@
+// internal/domain/valueobject/temperatura_test.go
+package valueobject
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTemperatura_Valor(t *testing.T) {
+	tests := []struct {
+		name string
+		temp Temperatura
+		want int
+	}{
+		{"60C", Temp60, 60},
+		{"75C", Temp75, 75},
+		{"90C", Temp90, 90},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, tt.temp.Valor())
+		})
+	}
+}
