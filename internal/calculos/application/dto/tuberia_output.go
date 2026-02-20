b3cffa1ecfd235c1d8184e95c341c62269528a74
GARFEX33 <ingenieriagarfex33@gmail.com>
2026-02-19 20:57:09 -0700

    refactor: remove unused SeleccionarTemperaturaPort, fix tuberia_output

--- internal/calculos/application/dto/tuberia_output.go
+++ internal/calculos/application/dto/tuberia_output.go
@@ -1,10 +1,6 @@
 // internal/calculos/application/dto/tuberia_output.go
 package dto
 
-import (
-	"github.com/garfex/calculadora-filtros/internal/calculos/domain/entity"
-)
-
 // TuberiaOutput contiene el resultado del cálculo de tamaño de tubería.
 // Es el DTO de salida para el use case CalcularTamanioTuberia.
 type TuberiaOutput struct {
@@ -13,15 +9,4 @@ type TuberiaOutput struct {
 	DesignacionMetrica string  `json:"designacion_metrica"`
 	TipoCanalizacion   string  `json:"tipo_canalizacion"`
 	NumTuberias        int     `json:"num_tuberias"`
-}
-
-// TuberiaOutputFromDomain convierte entity.ResultadoTamanioTuberia a TuberiaOutput.
-func TuberiaOutputFromDomain(r entity.ResultadoTamanioTuberia) TuberiaOutput {
-	return TuberiaOutput{
-		AreaPorTuboMM2:     r.AreaPorTuboMM2(),
-		TuberiaRecomendada: r.TuberiaRecomendada(),
-		DesignacionMetrica: r.DesignacionMetrica(),
-		TipoCanalizacion:   string(r.TipoCanalizacion()),
-		NumTuberias:        r.NumTuberias(),
-	}
 }
